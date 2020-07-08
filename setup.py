03ed1ec2bec213c084038ef070d609691bd16275
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-07-08 23:20:28 +0200

    Better package description in setup

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'pacmill',
-    version          = '0.1.0',
-    description      = 'pacmill XYZ.',
+    version          = '0.1.1',
+    description      = 'The `pacmill` python package is a bioinformatics '
+                       'pipeline that is developed to process microbial '
+                       '16S amplicon sequencing data.',
     license          = 'MIT',
     url              = 'http://github.com/xapple/pacmill/',
     author           = 'Lucas Sinclair',
     author_email     = 'lucas.sinclair@me.com',
     packages         = find_packages(),
-    install_requires = ['plumbing>=2.8.1', 'autopaths>=1.4.2', 'fasta>=2.0.2',
+    install_requires = ['plumbing>=2.8.1', 'autopaths>=1.4.2', 'fasta>=2.0.4',
                         'pymarktex>=1.4.0'],
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
