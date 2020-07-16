4278843e33d831a06c2b67cc4f809aef94ec1cb9
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-07-17 00:04:22 +0200

    Added tests for primer filtering

---
+++ test/conftest.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Written by Lucas Sinclair.
+MIT Licensed.
+Contact at www.sinclair.bio
+"""
+
+# Built-in modules #
+
+# Internal modules #
+from pacmill.core.project         import Project
+from pacmill.core.sample          import Sample
+from pacmill.filtering.seq_filter import SeqFilter
+
+# Third party modules #
+import mock, pytest
+
+###############################################################################
+@pytest.fixture
+# Remove the constructor #
+@mock.patch.object(Project, '__init__', lambda *args: None)
+def project(tmp_path):
+    """Returns a fake Project object ready to be used."""
+    project = Project('pytest_project')
+    # Set the name #
+    project.short_name = 'pytest_project'
+    # Return #
+    return project
+
+###############################################################################
+@pytest.fixture
+# Remove some methods #
+@mock.patch.object(Sample, 'transform_attrs', lambda *args: None)
+@mock.patch.object(Sample, 'validate_attrs',  lambda *args: None)
+def sample(project, tmp_path):
+    """Returns a fake Sample object ready to be used."""
+    # Remove the constructor and instantiate #
+    sample = Sample(project)
+    # Set the name #
+    sample.short_name = 'pytest_sample'
+    # Set the output dir #
+    sample.output_dir = str(tmp_path)
+    # Return #
+    return sample
+
+###############################################################################
+@pytest.fixture
+def seq_filter(sample):
+    """Returns a fake SeqFilter object ready to be used."""
+    # Make a mock SeqFilter object #
+    seq_filter = SeqFilter(sample)
+    # Return #
+    return seq_filter
---
+++ test/test_primers.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Written by Lucas Sinclair.
+MIT Licensed.
+Contact at www.sinclair.bio
+
+Script to test the parsing and filtering of primers in sequence data.
+"""
+
+# Built-in modules #
+
+# Internal modules #
+
+# First party modules #
+from fasta.primers import TwoPrimers
+from fasta import FASTQ
+
+# Third party modules #
+import pytest, Bio
+from Bio.SeqRecord import SeqRecord
+
+###############################################################################
+cases = [{
+    'fwd_primer': "ATTTA",                         # This is a standard case
+    'rev_primer':                     "AGGGA",     # Only forward and reverse
+    'seq':        "ATTTAGGGGGGGCGGGGGGGTCCCT",     # primers are found
+    'expected':        "GGGGGGGCGGGGGGG",
+}, {
+    'fwd_primer': "AAA",                           # Here both are found but
+    'rev_primer': "TTT",                           # they are found in the same
+    'seq':        "AAAAAAAA",                      # spot causing a 0 length
+}, {
+    'fwd_primer':                     "AATAA",     # The sequence is in the
+    'rev_primer': "AAAAA",                         # opposite direction
+    'seq':        "AAAAAGGGGGGCGCGGGGGGTTATT",     # (reverse complement)
+    'expected':        "CCCCCCGCGCCCCCC",
+}, {
+    'fwd_primer': "ATTTA",                         # Mismatch in the reverse
+    'rev_primer':                     "AGGGA",     # primer
+    'seq':        "ATTTAGGGGGGGCGGGGGGGTCTCT",
+}, {
+    'fwd_primer': "ATTTA",                        # Authorizing a mismatch here
+    'rev_primer':                     "AGGGA",    # causes the reverse primer
+    'seq':        "ATTTAGGGGGGGCGGGGGGGTCCCT",    # to be found twice at pos
+    'mismatches': 1                               # 4 and at pos 20
+}, {
+    'fwd_primer': "ATTAATTA",
+    'rev_primer':                    "AGAAGA",    # Authorizing a mismatch here
+    'seq':        "TTTAATTAGGGGCGGGGGGTCTTTT",    # causes both primers to be
+    'expected':           "GGGGCGGGGGG",          # found correctly
+    'mismatches': 1
+}, {
+    'fwd_primer':    "ATTAATTA",
+    'rev_primer':                       "AGAAGA",     # Primers are further
+    'seq':        "GGGTTTAATTAGGGGCTGGGGGTCTTTTGGG",  # inside the read but
+    'expected':              "GGGGCTGGGGG",           # are found correctly
+    'mismatches': 1
+}]
+
+input_names = ('fwd_primer', 'rev_primer', 'seq', 'expected', 'mismatches')
+params = [[case.get(name) for name in input_names] for case in cases]
+
+###############################################################################
+@pytest.mark.parametrize(input_names, params)
+def test_primers(seq_filter, fwd_primer, rev_primer, seq, expected,
+                 mismatches):
+    # Pytest doesn't support default values #
+    if mismatches is None: mismatches = 0
+    # Create primers #
+    primers = TwoPrimers(fwd_primer, rev_primer)
+    # Create the sequences for the  fake FASTQ #
+    reads = [Bio.SeqRecord.SeqRecord(Bio.Seq.Seq(seq), id='pytest_read')]
+    # Make a fake FASTQ #
+    fastq = FASTQ('/dummy_path.fastq')
+    # Set some attributes (monkeypatch) #
+    fastq.parse = lambda: reads
+    fastq.count = 1
+    # Call the primer parsing routines #
+    reads_w_primers = fastq.parse_primers(primers, mismatches=mismatches)
+    # Get the result of the function #
+    result = seq_filter.primer_gen(reads_w_primers)
+    # Case where the sequence should be eliminated #
+    if expected is None:
+        with pytest.raises(StopIteration): next(result)
+        return
+    # Get the result #
+    output = str(next(result).seq)
+    # Assert #
+    assert output == expected
