a58eb0b3126fa79cc289465a01642fc2e2379c5a
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-08-25 23:41:02 +0200

    Next version of seqsearch

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email     = 'lucas.sinclair@me.com',
     packages         = find_packages(),
     install_requires = ['plumbing>=2.8.9', 'autopaths>=1.4.6', 'fasta>=2.1.6',
-                        'pymarktex>=1.4.6', 'seqsearch>=1.2.5' 'biopython',
+                        'pymarktex>=1.4.6', 'seqsearch>=1.2.7' 'biopython',
                         'pandas', 'sh', 'tag', 'shell_command', 'tabulate'],
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
