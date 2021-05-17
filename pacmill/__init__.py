ee93a4f225de19970a47dd7fe3c6d7eb41de7da8
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-05-17 21:39:36 +0200

    Remove the check dependencies

--- pacmill/__init__.py
+++ pacmill/__init__.py
@@ -8,7 +8,7 @@ Contact at www.sinclair.bio
 """
 
 # Special variables #
-__version__ = '0.5.4'
+__version__ = '0.5.5'
 
 # Built-in modules #
 import os, sys
@@ -30,9 +30,3 @@ repos_dir = module_dir.directory
 
 # The module is maybe in a git repository #
 git_repo = GitRepo(repos_dir, empty=True)
-
-# If we are in development mode, check dependencies #
-setup_py = repos_dir + 'setup.py'
-if setup_py.exists:
-    from plumbing.dependencies import check_setup_py
-    check_setup_py(setup_py)
