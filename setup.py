e120e13a4209be43fc8e647a303881904080fdf4
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-05-17 20:39:40 +0200

    Better discovery of packages for distribution

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name             = 'pacmill',
@@ -10,11 +10,11 @@ setup(
     url              = 'http://github.com/xapple/pacmill/',
     author           = 'Lucas Sinclair',
     author_email     = 'lucas.sinclair@me.com',
-    packages         = find_packages(),
+    packages         = find_namespace_packages(),
     install_requires = ['plumbing>=2.9.4', 'autopaths>=1.4.6', 'fasta>=2.2.2',
                         'pymarktex>=1.4.6', 'seqsearch>=1.3.3', 'biopython',
                         'pandas', 'sh', 'tag', 'shell_command', 'tabulate',
-                        'openpyxl'],
+                        'openpyxl', 'mock'],
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
     include_package_data = True,
