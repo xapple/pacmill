ade7050df33c34e00582e7a6b545fb1c8bea51cb
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-07-14 16:36:47 +0200

    Show a different customizable hostname in reports

--- pacmill/reports/base_template.py
+++ pacmill/reports/base_template.py
@@ -8,6 +8,7 @@ Contact at www.sinclair.bio
 """
 
 # Built-in modules #
+import os
 import socket
 
 # Internal modules #
@@ -25,7 +26,11 @@ class ReportTemplate(Template):
     def project_url(self):       return pacmill.project_url
     def project_version(self):   return pacmill.__version__
     def now(self):               return pretty_now()
-    def hostname(self):          return socket.gethostname()
+
+    def hostname(self):
+        host = os.environ.get('PACMILL_HOSTNAME')
+        if host is not None: return host
+        return socket.gethostname()
 
     def git(self):
         if not pacmill.git_repo: return False
